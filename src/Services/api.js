60ba95f09f42aeaa242a255235174a30279cb15d
Kaspeodor <marcusvini16051998@gmail.com>
2025-02-08 09:33:17 -0300

    set-configs

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
+
 const api = axios.create ({
     baseURL: 'https://api.themoviedb.org/3/',
-    api_key: '9ec3abea25bbb90dd3f5f04cde2fab9c',
-    language: 'pt-BR',
-    page: 1
+    params : {
+        api_key: '9ec3abea25bbb90dd3f5f04cde2fab9c',
+        language: 'pt-BR',
+        page: 1
+    }
+    
 })
+// https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+
+export default api
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,5 +1,16 @@
-
+import api from "../../Services/api"
 const Home = () => {
+    
+     const getMovies = async () =>{
+
+        const data = await api.get ('/movie/popular')
+
+        console.log (data)
+
+    }
+
+    getMovies ()
+
     return (
         
         <div>
